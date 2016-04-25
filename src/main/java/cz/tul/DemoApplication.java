7f33e04b56518ec0a5059045478d240b7e017f1b
petr-bartos92 <petr.bartos92@gmail.com>
2016-04-25 14:30:21 +0200

    Initial
    Initial push

---
+++ src/main/java/cz/tul/DemoApplication.java
@@ -0,0 +1,12 @@
+package cz.tul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
