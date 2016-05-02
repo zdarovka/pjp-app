513ca404c06a0694baadb47b1a00e8223ccf7831
Petr Zdarsky <petr.zdarsky@praguelabs.com>
2016-05-02 15:36:52 +0200

    Added Comment entity.

--- src/main/java/cz/tul/data/Author.java
+++ src/main/java/cz/tul/data/Author.java
@@ -38,6 +38,4 @@ public class Author {
     public void setDateCreated(Date dateCreated) {
         this.dateCreated = dateCreated;
     }
-
-
 }
--- src/main/java/cz/tul/data/Picture.java
+++ src/main/java/cz/tul/data/Picture.java
@@ -29,9 +29,4 @@ public class Picture {
 
     @Column(name = "dislikes")
     private int dislikes;
-
-
-
-
-
 }
