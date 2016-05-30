d9a82f9ec4c0b3121af84f69e90f723d970a3628
Petr Zdarsky <petr.zdarsky@praguelabs.com>
2016-05-30 02:37:42 +0200

    Prepared controllers.

---
+++ src/main/java/cz/tul/api/AuthorsController.java
@@ -0,0 +1,8 @@
+package cz.tul.api;
+
+/**
+ * Created by zdars on 30.05.2016.
+ */
+
+public class AuthorsController extends ApiBaseController{
+}
---
+++ src/main/java/cz/tul/controller/BaseController.java
@@ -0,0 +1,10 @@
+package cz.tul.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by zdars on 30.05.2016.
+ */
+@Controller
+public abstract class BaseController {
+}
