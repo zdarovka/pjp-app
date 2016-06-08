02aea6d884ab3822051f9cff4db4f8d679664201
Petr Zdarsky <petr.zdarsky@praguelabs.com>
2016-06-08 17:45:37 +0200

    Removed unused imports.

--- src/main/java/cz/tul/configuration/JpaConfig.java
+++ src/main/java/cz/tul/configuration/JpaConfig.java
@@ -1,9 +1,5 @@
 package cz.tul.configuration;
 
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.test.ImportAutoConfiguration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
