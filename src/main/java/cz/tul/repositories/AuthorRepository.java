eccb12bd723d1b38fdca1287afb785dea24b51a2
Petr Zdarsky <petr.zdarsky@praguelabs.com>
2016-06-08 15:08:18 +0200

    Mysql prep.

--- src/main/java/cz/tul/repositories/AuthorRepository.java
+++ src/main/java/cz/tul/repositories/AuthorRepository.java
@@ -4,7 +4,9 @@ import cz.tul.data.Author;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AuthorRepository extends CrudRepository<Author, String> {
-
+    List<Author> findAll();
 }
