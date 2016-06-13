4852585944f481b4fe5eaacf855a41763133db67
Petr Zdarsky <petr.zdarsky@praguelabs.com>
2016-06-13 17:00:11 +0200

    Fixed merge

--- src/main/java/cz/tul/client/ServerApi.java
+++ src/main/java/cz/tul/client/ServerApi.java
@@ -1,24 +1,13 @@
 package cz.tul.client;
 
-<<<<<<< HEAD
-/**
- * Created by zdars on 6/13/2016.
- */
-public class ServerApi {
-=======
 
 public interface ServerApi {
 
     public static final String UPLOAD_PATH = "/api/upload/{name}";
 
-    public static final String PICTURES_PATH = "/api/pictures";
-    public static final String PICTURE_PATH = PICTURES_PATH + "/{id}";
->>>>>>> 0aefea0662892bd341c73ec70b6d2f6640547d36
 
     public static final String AUTHORS_PATH = "/api/authors";
     public static final String AUTHOR_PATH = AUTHORS_PATH + "/{id}";
-
-<<<<<<< HEAD
 
     public static final String COMMENTS_PATH = "/api/comments";
 
@@ -37,9 +26,7 @@ public interface ServerApi {
     public static final String PICTURES_BY_NAME_PATH = PICTURES_PATH + "/name/{name}";
     public static final String PICTURES_BY_AUTHOR_PATH = PICTURES_PATH + "/author/{id}";
     public static final String PICTURES_BY_TAG_PATH = PICTURES_PATH + "/tag/{name}";
-=======
-    public static final String COMMENTS_PATH = "/api/comments";
-    public static final String COMMENT_PATH = COMMENTS_PATH + "/{id}";
 
->>>>>>> 0aefea0662892bd341c73ec70b6d2f6640547d36
+
+
 }
